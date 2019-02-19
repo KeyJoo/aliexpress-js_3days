eccdf347320bb12fdd6d79c7830395d6bcab516d
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-02-19 19:14:21 +0300

    Add: set variables by class for DOM in index.html

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// Need Use ES6
+
+window.addEventListener('DOMContentLoaded', () => {
+  const cartWrapper = document.querySelector('.cart__wrapper'),
+    cart = document.querySelector('.cart'),
+    close = document.querySelector('.cart__close'),
+    open = document.querySelector('#cart'),
+    goodsBtn = document.querySelectorAll('.goods__btn'),
+    products = document.querySelectorAll('.goods__item'),
+    confirm = document.querySelector('.confirm'),
+    badge = document.querySelector('.nav__bage'),
+    totalCost = document.querySelector('.cart__total > span'),
+    titles = document.querySelectorAll('.goods__title');
+
+});
